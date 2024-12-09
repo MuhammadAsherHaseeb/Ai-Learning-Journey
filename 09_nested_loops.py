f081630e80b5bce5e881db888375c1926991b893
MuhammadAsherHaseeb <asherhaseebkhan@gmail.com>
2024-12-09 18:54:37 +0500

    day 25

---
+++ 09_nested_loops.py
@@ -0,0 +1,23 @@
+# Neested for loops 
+# colors = ["red", "green", "blue"]
+# items = ["pens", "notebooks", "pencils"]
+# for color in colors:
+#     for item in items:
+#         print(color, item)
+
+#NESTED WHILE LOOPS
+# i = 0
+# while i < 3:
+#     j = 0
+#     while j < 3:
+#         print(i, j)
+#         j = j + 1
+#     i = i + 1
+
+# For loop inside while loop
+i = 1
+while i < 4:
+    for j in range(3):
+        print(i, j)
+    i = i + 1
+    
