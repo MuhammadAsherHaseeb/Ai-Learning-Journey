d1952294f99f5c06451589156cdece5a0e0cda6b
MuhammadAsherHaseeb <asherhaseebkhan@gmail.com>
2024-12-09 18:55:10 +0500

    day 25

---
+++ 12_errors.py
@@ -0,0 +1,31 @@
+# x = 5
+
+# if x > 3:
+# print("x is a postive value")
+# syntax eror of intendation
+
+# Logical human error  y is not defined
+# x = 5
+
+# if x > 3:
+#     print("y is a postive value")
+
+# runtime or module not found because library not installed
+# print(np.sqrt(9))
+
+# syntax error
+# print("Hello")
+# print("World")    
+#name error
+
+#value error
+#type error
+#index error
+#key error
+#attribute error
+#zero division error (1/0)
+# file not found error
+# import error
+# memory error
+# timeout error
+ 
