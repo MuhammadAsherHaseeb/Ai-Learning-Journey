2b3363f9db8578ee0578d374244f159fee8969e7
MuhammadAsherHaseeb <asherhaseebkhan@gmail.com>
2024-12-09 18:53:30 +0500

    day25

---
+++ 05_input_program.py
@@ -0,0 +1,10 @@
+person_A_name = input("What is your name:")
+person_A_age = input("What is Your Age:")
+
+person_B_name = input("What is your name:")
+person_B_age = input("What is Your Age:")
+
+if person_A_age > person_B_age:
+    print(person_A_name, "is older than", person_B_name)
+else:
+    print(person_A_name, "is Younger than", person_B_name)
---
+++ 07_data_structure_indesing.py
@@ -0,0 +1,25 @@
+#Data Structure
+#List
+#define a list
+# food = ["Dahi Bhallay", "Biryani", "Daal", "samosay", "Shami", "Palak Paneer"]
+# print(food)
+# print(food[-1])
+# food[1] = "Chicken Pulao"
+# print(food[1])
+
+# 2 - Tupple
+
+# coordinates = (4.21, 9.29)
+# print(coordinates)
+# print(coordinates[0])
+# 3 - Set 
+
+# food_set = {"Dahi Bhallay", "Biryani", "Daal", "samosay", "Shami", "Palak Paneer"}
+# print(food_set)
+# food_set.add('pakora')
+
+# Dictionary
+car = {'brand': 'ford', 'model': 'Mustang', 'year':1964}
+print(car)
+car['year'] = 2023
+print(car['year'])
