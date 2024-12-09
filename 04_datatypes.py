2cf162bc5dbaec56fda2d6dc2fc4a3218bc1cf1c
MuhammadAsherHaseeb <asherhaseebkhan@gmail.com>
2024-12-09 18:26:57 +0500

    Day 1

---
+++ 04_datatypes.py
@@ -0,0 +1,22 @@
+x = 5 #Integer
+y = 5.0 # Float
+z = "salam" #String
+a = [1, 2, 3] # List of integrs
+a_1 = ["ali","ash","tayyab","usama"] # List of String
+b = (1,2,3) #Tuple
+c = {"ali":5, "ash":5.4, "tayyab":5.8, "usma":6.1} #Dictionary
+d = {1,2,3} #set
+print("HI")
+
+#Indentation
+a = 5
+b = 15
+c = 20.5
+print(a<b)
+if a < b:
+    print("a is less than b") #intended
+if c > b:
+    print("c is less than b") #intended
+else:
+    print("No b is greater than c") #intended
+
