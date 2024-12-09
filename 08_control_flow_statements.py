e1dd0e1cbb5b4554733605710ca1d8889d00fe14
MuhammadAsherHaseeb <asherhaseebkhan@gmail.com>
2024-12-09 18:54:20 +0500

    day 25

---
+++ 08_control_flow_statements.py
@@ -0,0 +1,34 @@
+# Conditional statement
+# x = -11
+# if x > 0:
+#     print("x is positive")
+# elif x < 0:
+#     print("x is negative")
+# else:
+#     print("x is zero")
+
+# For loop
+# menu = ["Dahi Bhallay", "Biryani", "Daal", "samosay", "Shami", "Palak Paneer"]
+# for food in menu:
+#     print(food)
+
+# While loop 
+# i = 1
+# while i < 100:
+#     print(i)
+#     i = i+1
+
+# for letters in "python":
+#     if letters == "h":
+#         break
+#     print(letters)
+
+# for letters in "python":
+#     if letters == "h":
+#         continue
+#     print(letters)
+
+for letters in "python":
+    if letters == "h":
+        pass
+    print(letters)
